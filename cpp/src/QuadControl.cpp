46f2233843c02c5be1ebd2eeb7f5cf9c20e4e17b
CDA <daemschris@gmail.com>
2018-04-29 22:03:15 +0200

    roll pitch

--- cpp/src/QuadControl.cpp
+++ cpp/src/QuadControl.cpp
@@ -163,8 +163,8 @@ V3F QuadControl::RollPitchControl(V3F accelCmd, Quaternion<float> attitude, floa
     //// rollPitchCmd.x = (R21 * bxCommandedDot - R11 * byCommandedDot) / R33
     //// rollPitchCmd.y = (R22 * bxCommandedDot - R12 * byCommandedDot) / R33
     //// rollPitchCmd.z = 0.0;
-    pqrCmd.x = (R(1,0) * b_x_commanded_dot - R(1,1) * b_y_commanded_dot) / R(2,2);
-    pqrCmd.y = (R(1,1) * b_x_commanded_dot - R(0,1) * b_y_commanded_dot) / R(2,2);
+    pqrCmd.x = (R(1,0) * b_x_commanded_dot - R(0,0) * b_y_commanded_dot) * (1 / R(2,2));
+    pqrCmd.y = (R(1,1) * b_x_commanded_dot - R(0,1) * b_y_commanded_dot) * (1 / R(2,2));
     pqrCmd.z = 0.0;
     
     
@@ -205,10 +205,10 @@ float QuadControl::AltitudeControl(float posZCmd, float velZCmd, float posZ, flo
     float p_term = kpPosZ * z_error;
     float d_term = kpVelZ * z_error_dot;
     
-    //integratedAltitudeError = integratedAltitudeError + (z_error * dt);
-    //float i_term = KiPosZ * integratedAltitudeError;
+    integratedAltitudeError = integratedAltitudeError + (z_error * dt);
+    float i_term = KiPosZ * integratedAltitudeError;
     
-    float u_1_bar = p_term + d_term + accelZCmd; // + i_term;
+    float u_1_bar = p_term + d_term + accelZCmd + i_term;
     
     float acceleration = (u_1_bar - CONST_GRAVITY) / b_z;
     
